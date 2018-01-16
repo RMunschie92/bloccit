ab39bfda5f6462c297894b7489e5ba3689376249
Ryan Munsch <rmunsch9632@gmail.com>
2018-01-15 19:31:10 -0500

    assignment 23 work completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,22 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase if email.present? }
+  before_save {
+    if name
+      name = self.name.split(' ')
+      new_name = []
+      name.each do |word|
+        word.capitalize!
+        new_name << word
+      end
+      new_name = new_name.join(' ')
+      self.name = new_name
+    end
+  }
 
-  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+  validates :name,
+            length: { minimum: 1, maximum: 100 },
+            presence: true,
+            uniqueness: { case_sensitive: false }
 
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe User, type: :model do
      # Shoulda tests for name
      it { is_expected.to validate_presence_of(:name) }
      it { is_expected.to validate_length_of(:name).is_at_least(1) }
+     it { is_expected.to validate_uniqueness_of(:name) }
+     it { is_expected.to allow_value("Bloccit User").for(:name) }
 
      # Shoulda tests for email
      it { is_expected.to validate_presence_of(:email) }
