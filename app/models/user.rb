035774875171a3fc0c5f263848325d888b78ab42
Ryan Munsch <rmunsch9632@gmail.com>
2018-01-18 00:26:04 -0500

    checkpoint 26 work completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
   before_save { self.email = email.downcase if email.present? }
 
-  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+  validates :name,
+            length: { minimum: 1, maximum: 100 },
+            presence: true,
+            uniqueness: { case_sensitive: false }
 
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,15 @@
 
 require 'random_data'
 
+5.times do
+  User.create!(
+    name: RandomData.random_name,
+    email: RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+end
+users= User.all
+
 # Create Topics
 15.times do
   Topic.create!(
@@ -16,6 +25,7 @@ topics = Topic.all
 # Create Posts
 50.times do
   Post.create!(
+    user: users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
@@ -32,7 +42,14 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.update_attributes!(
+  email: 'rmunsch9632@gmail.com',
+  password: 'helloword'
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
